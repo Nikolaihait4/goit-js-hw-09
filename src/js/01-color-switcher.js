4116441ce259f2faecdb2a0761b4314afbfa8dca
Nikolaihait4 <jet1nikolai@gmail.com>
2023-08-01 22:33:12 +0200

    task - 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const CHANGE_COLOR_DELAY = 1000;
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onClickStart() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function onClickStop() {
+  clearInterval(timerId);
+}
