7e53855f10955d9321d8b99fa212ffdae43f95b2
Nikolaihait4 <jet1nikolai@gmail.com>
2023-08-02 11:22:42 +0200

    task-1 futurebutton

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,12 @@
 const CHANGE_COLOR_DELAY = 1000;
+let timerId = null;
 const refs = {
   start: document.querySelector('button[data-start]'),
   stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
 };
 
-let timerId = null;
-
+refs.stop.disabled = true;
 refs.start.addEventListener('click', onClickStart);
 refs.stop.addEventListener('click', onClickStop);
 
@@ -16,11 +17,16 @@ function getRandomHexColor() {
 }
 
 function onClickStart() {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+
   timerId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
+    refs.body.style.backgroundColor = getRandomHexColor();
   }, CHANGE_COLOR_DELAY);
 }
 
 function onClickStop() {
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
   clearInterval(timerId);
 }
