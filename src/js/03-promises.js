ccf590cc07262c2c8418857aee20799b14488362
Nikolaihait4 <jet1nikolai@gmail.com>
2023-08-04 20:13:37 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+// Обработчик события на форме
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // Получение значений полей формы
+  const delay = parseInt(document.querySelector('input[name="delay"]').value);
+  const step = parseInt(document.querySelector('input[name="step"]').value);
+  const amount = parseInt(document.querySelector('input[name="amount"]').value);
+
+  // Создание промисов с задержкой и отображение уведомлений
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + i * step;
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
